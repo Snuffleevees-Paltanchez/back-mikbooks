433edb80cb6ab386daab729f4f306d827b53979a
Ria Deane <62292336+riadeane@users.noreply.github.com>
2024-06-20 16:57:07 -0400

    feat: get book endpoints (#10)
    * feat(books): add getAll (w/filters) & getById methods
    
    * feat(books): add get all and get id endpoints
    
    * feat(books): add controller to module
    
    * feat(books): add typing and getbyISBN
    
    * feat(app): add transform: true to validationpipe

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,25 +1,51 @@
 import {
   Controller,
+  Get,
   Put,
   Delete,
   Param,
+  Query,
   Body,
   NotFoundException,
-  ValidationPipe,
 } from '@nestjs/common'
 import { BookService } from './book.service'
-import { BookDto } from './dto'
+import { BookDto, GetBooksDto, GetBookByISBNDto } from './dto'
 
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  @Get()
+  async getAllBooks(@Query() query: GetBooksDto) {
+    const { page, limit, ...filters } = query
+    const books = await this.bookService.getAllBooks(page, limit, filters)
+    if (!books || books.total === 0) {
+      throw new NotFoundException('No books found')
+    }
+    return books
+  }
+
+  @Get(':id')
+  async getBookById(@Param('id') id: number) {
+    const book = await this.bookService.getBookById(id)
+    if (!book) {
+      throw new NotFoundException(`Book with id ${id} not found`)
+    }
+    return book
+  }
+
+  @Get('isbn/:isbn')
+  async getBookByIsbn(@Param() { isbn }: GetBookByISBNDto) {
+    const book = await this.bookService.getBookByISBN(isbn)
+    if (!book) {
+      throw new NotFoundException(`Book with isbn ${isbn} not found`)
+    }
+    return book
+  }
+
   @Put(':id')
-  async updateBook(
-    @Param('id') id: string,
-    @Body(new ValidationPipe()) bookDto: BookDto,
-  ) {
-    const updatedBook = await this.bookService.updateBook(parseInt(id, 10), bookDto)
+  async updateBook(@Param('id') id: number, @Body() bookDto: BookDto) {
+    const updatedBook = await this.bookService.updateBook(id, bookDto)
     if (!updatedBook) {
       throw new NotFoundException(`Book with id ${id} not found`)
     }
--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
 import { BookService } from './book.service'
+import { BookController } from './book.controller'
 @Module({
+  controllers: [BookController],
   providers: [BookService],
   exports: [BookService],
 })
