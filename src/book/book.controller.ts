5264a36105cf7ccf1886ddfe0d4bb88942456c98
bastian <b.marinkovic@uc.cl>
2024-06-21 00:51:59 -0400

    feat(docs): add docs to book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -9,13 +9,25 @@ import {
   NotFoundException,
 } from '@nestjs/common'
 import { BookService } from './book.service'
-import { BookDto, GetBooksDto, GetBookByISBNDto } from './dto'
+import { BookDto, GetBooksDto, GetBookByISBNDto, BooksResponse } from './dto'
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger'
 
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Get()
+  @ApiTags('Books')
+  @ApiOperation({ summary: 'Get all books' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of books',
+    type: BooksResponse,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No books found',
+  })
   async getAllBooks(@Query() query: GetBooksDto) {
     const { page, limit, ...filters } = query
     const books = await this.bookService.getAllBooks(page, limit, filters)
@@ -26,6 +38,10 @@ export class BookController {
   }
 
   @Get(':id')
+  @ApiTags('Books')
+  @ApiOperation({ summary: 'Get book by id' })
+  @ApiResponse({ status: 200, description: 'Book found', type: BookDto })
+  @ApiResponse({ status: 404, description: 'Book not found' })
   async getBookById(@Param('id') id: number) {
     const book = await this.bookService.getBookById(id)
     if (!book) {
@@ -35,6 +51,10 @@ export class BookController {
   }
 
   @Get('isbn/:isbn')
+  @ApiTags('Books')
+  @ApiOperation({ summary: 'Get book by ISBN' })
+  @ApiResponse({ status: 200, description: 'Book found', type: BookDto })
+  @ApiResponse({ status: 404, description: 'Book not found' })
   async getBookByIsbn(@Param() { isbn }: GetBookByISBNDto) {
     const book = await this.bookService.getBookByISBN(isbn)
     if (!book) {
@@ -44,6 +64,10 @@ export class BookController {
   }
 
   @Put(':id')
+  @ApiTags('Books')
+  @ApiOperation({ summary: 'Update book by id' })
+  @ApiResponse({ status: 200, description: 'Book updated', type: BookDto })
+  @ApiResponse({ status: 404, description: 'Book not found' })
   async updateBook(@Param('id') id: number, @Body() bookDto: BookDto) {
     const updatedBook = await this.bookService.updateBook(id, bookDto)
     if (!updatedBook) {
@@ -53,6 +77,10 @@ export class BookController {
   }
 
   @Delete(':id')
+  @ApiTags('Books')
+  @ApiOperation({ summary: 'Delete book by id' })
+  @ApiResponse({ status: 200, description: 'Book deleted', type: BookDto })
+  @ApiResponse({ status: 404, description: 'Book not found' })
   async deleteBook(@Param('id') id: number) {
     const deletedBook = await this.bookService.deleteBook(id)
     if (!deletedBook) {
--- src/book/dto/book.dto.ts
+++ src/book/dto/book.dto.ts
@@ -13,73 +13,88 @@ import {
 } from 'class-validator'
 
 import { Type } from 'class-transformer'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class BookDto {
   @IsNotEmpty()
   @IsString()
   @MaxLength(255)
+  @ApiProperty({ example: 'Book Title' })
   title: string
 
   @IsNotEmpty()
   @IsNumber()
   @Type(() => Number)
+  @ApiProperty({ example: 1 })
   authorId: number
 
   @IsNotEmpty()
   @IsISBN()
   @MaxLength(13)
+  @ApiProperty({ example: '9783161484100' })
   isbn: string
 
   @IsOptional()
   @IsDate()
+  @ApiProperty({ example: '2021-01-01', nullable: true })
   publishedDate: Date | null
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Book description', nullable: true })
   description: string | null
 
   @IsOptional()
   @IsUrl()
   @MaxLength(255)
+  @ApiProperty({ example: 'https://example.com/book.jpg', nullable: true })
   imgUrl: string | null
 
   @IsOptional()
   @IsArray()
+  @ApiProperty({ example: ['Fiction', 'Thriller'], nullable: true })
   categories: string[] | null
 }
 
 export class BookFilterDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({ required: false, example: 'Filter by  Title' })
   title?: string
 
   @IsOptional()
   @IsInt()
   @Type(() => Number)
+  @ApiProperty({ required: false, description: 'Filter by author ID' })
   authorId?: number
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ required: false, description: 'Filter by author name' })
   authorName?: string
 
   @IsOptional()
   @IsISBN()
+  @ApiProperty({ required: false, description: 'Filter by ISBN' })
   isbn?: string
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ required: false, description: 'Filter by category' })
   category?: string
 
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   @Min(0)
+  @ApiProperty({ required: false, description: 'Filter by minimum price' })
   minPrice?: number
 
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   @Min(0)
+  @ApiProperty({ required: false, description: 'Filter by maximum price' })
   maxPrice?: number
 }
 
@@ -88,12 +103,14 @@ export class GetBooksDto extends BookFilterDto {
   @IsNumber()
   @Type(() => Number)
   @Min(1)
+  @ApiProperty({ required: false, description: 'Page number' })
   page?: number
 
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   @Min(1)
+  @ApiProperty({ required: false, description: 'Number of items per page' })
   limit?: number
 }
 
@@ -134,4 +151,18 @@ export class UpdateBookDto {
   @IsOptional()
   @IsArray()
   categories?: string[] | null
+}
+
+export class BooksResponse {
+  @ApiProperty()
+  total: number
+
+  @ApiProperty()
+  page: number
+
+  @ApiProperty()
+  limit: number
+
+  @ApiProperty({ type: [BookDto] })
+  data: BookDto[]
 }
