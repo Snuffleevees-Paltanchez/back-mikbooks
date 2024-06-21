d84049ba22e37dfaee0cfe6ddd825327296cd81b
bastian <b.marinkovic@uc.cl>
2024-06-21 00:49:37 -0400

    feat(docs): add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 function checkEnvironment(configService: ConfigService) {
   const requiredEnvVars = ['DATABASE_URL', 'ISSUER_BASE_URL', 'AUDIENCE']
@@ -18,6 +19,16 @@ async function bootstrap() {
 
   const configService = app.get<ConfigService>(ConfigService)
   checkEnvironment(configService)
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   await app.listen(3000)
